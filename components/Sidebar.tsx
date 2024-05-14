79209b603cd462503a55474c21528245d7079759
Guilherme Afonso <profguilherme.as@gmail.com>
2024-05-14 17:47:23 -0300

    feat: create sidebar components files

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 const Sidebar = () => {
     const pathname = usePathname()
@@ -14,14 +15,19 @@ const Sidebar = () => {
                 {sidebarLinks.map((link) => {
                     const isActive = pathname === link.route || pathname.startsWith(link.route)
                     return (
-                        <Link 
-                        href={link.route}
-                        key={link.label}
-                        className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
-                            'bg-blue-1': isActive,
-                        })}
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+                                'bg-blue-1': isActive,
+                            })}
                         >
-                            {link.label}
+                            <Image 
+                                src={link.imgUrl} 
+                                alt={link.label} 
+                                width={24} 
+                                height={24}/>
+                            <p className='text-lg font-semibold max-lg:hidden'>{link.label}</p>
                         </Link>
                     )
                 })}
